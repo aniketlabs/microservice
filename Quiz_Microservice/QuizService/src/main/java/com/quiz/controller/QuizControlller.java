96ba896e6ae333348add77909b17ef1de769ab6b
Aniket Narayan Shinde <aniketnarayanshinde@Anikets-MacBook-Air.local>
2026-01-01 13:28:26 +0530

    Move all microservices under Quiz_Microservice folder

---
+++ Quiz_Microservice/QuizService/src/main/java/com/quiz/controller/QuizControlller.java
@@ -0,0 +1,53 @@
+package com.quiz.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.entities.Quiz;
+import com.quiz.service.QuestionClient;
+import com.quiz.service.QuizService;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizControlller {
+	
+	
+	private QuizService quizService;
+	private QuestionClient questionClient;
+
+	
+	
+	
+	public QuizControlller(QuizService quizService, QuestionClient questionClient) {
+		super();
+		this.quizService = quizService;
+		this.questionClient = questionClient;
+	}
+
+	@PostMapping
+	public Quiz create(@RequestBody Quiz quiz) {
+		return quizService.add(quiz);
+				
+	}
+	
+	@GetMapping
+	public List<Quiz> getAll(){
+		return quizService.get();
+	}
+	
+	@GetMapping("/{id}")
+	public Quiz getOne(@PathVariable Long id) {
+		return quizService.get(id);
+	}
+	
+	
+	
+	
+
+}
